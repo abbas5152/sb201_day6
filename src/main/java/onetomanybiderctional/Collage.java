e6d37a69f3dd2e85b518102d91efce7a14fd5051
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-10 13:33:21 +0530

    Create Collage.java

---
+++ src/main/java/onetomanybiderctional/Collage.java
@@ -0,0 +1,71 @@
+package onetomany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Collage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int collageId;
+	private String collageName;
+	private String collageAddress;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy ="collage")
+	private List<Student> student=new ArrayList<Student>();
+
+	public int getCollageId() {
+		return collageId;
+	}
+
+	public void setCollageId(int collageId) {
+		this.collageId = collageId;
+	}
+
+	public String getCollageName() {
+		return collageName;
+	}
+
+	public void setCollageName(String collageName) {
+		this.collageName = collageName;
+	}
+
+	public String getCollageAddress() {
+		return collageAddress;
+	}
+
+	public void setCollageAddress(String collageAddress) {
+		this.collageAddress = collageAddress;
+	}
+
+	public List<Student> getStudent() {
+		return student;
+	}
+
+	public void setStudent(List<Student> student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return "Collage [collageId=" + collageId + ", collageName=" + collageName + ", collageAddress=" + collageAddress
+				+ ", student=" + student + "]";
+	}
+
+	public Collage() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
