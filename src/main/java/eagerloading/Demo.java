b82400007b4c2dc9b81cfdb0c32db3226d51127c
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-10 13:19:27 +0530

    Add files via upload

---
+++ src/main/java/eagerloading/Demo.java
@@ -0,0 +1,47 @@
+package EagerLoading;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		EntityManager em= Persistence.createEntityManagerFactory("clientUnit").createEntityManager();
+		
+		   Query q=em.createQuery("from Customer where cid=?1");
+		   q.setParameter(1, 1);
+		   
+		   List<Customer> c =q.getResultList();
+				   
+		
+		em.close();
+		
+            for(Customer cus:c) {
+            	
+           	 System.out.println(cus.getCid());
+        	 System.out.println(cus.getName());
+        	 System.out.println(cus.getEmail());
+        	 System.out.println(cus.getMobileNumber());
+        	 List<Address> adrs = cus.getAddresses();
+        	 for(Address a:adrs) {
+        		 System.out.println(a.getState());
+        		 System.out.println(a.getCity());
+        		 System.out.println(a.getPincode());
+        	 }
+            }
+	 
+	 
+	 
+	
+	 
+
+		
+		
+		
+		
+	}
+
+}
